186c48c7fd7415a1882485ba50d6d931efbc9260
Hamlet Simona <zeonBPM@gmail.com>
2025-09-28 13:09:14 -0300

    Update GBAPoisonNonLethal.py

--- Components/Pending Sorting/GBAPoisonNonLethal.py
+++ Components/Pending Sorting/GBAPoisonNonLethal.py
@@ -8,8 +8,7 @@ class GBAPoisonNonLethal(SkillComponent):
 
     def on_upkeep(self, actions, playback, unit):
         old_random_state = static_random.get_combat_random_state()
-        hp_loss = -static_random.get_randint(1, self.value)
+        hp_loss = max(-static_random.get_randint(1, self.value), -unit.get_hp()+1) #Make damage non-lethal
         new_random_state = static_random.get_combat_random_state()
         actions.append(action.RecordRandomState(old_random_state, new_random_state))
-        hp_loss = min(hp_loss, unit.get_hp()-1) #Make damage non-lethal
         actions.append(action.ChangeHP(unit, hp_loss))
